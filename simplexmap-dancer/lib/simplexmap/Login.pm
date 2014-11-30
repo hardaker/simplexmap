48360c85021ad240eb3e9eabe71eebdaea606f1b
Wes Hardaker <opensource@hardakers.net>
2014-11-30 08:08:51 -0800

    basis of a login system

--- simplexmap-dancer/lib/simplexmap/Login.pm
+++ simplexmap-dancer/lib/simplexmap/Login.pm
@@ -2,11 +2,19 @@ package simplexmap::Login;
 
 use Dancer ':syntax';
 use Dancer::Plugin::Database;
-
+use Dancer::Plugin::DataFormValidator;
+use Data::FormValidator::Constraints qw(:closures);
+use MIME::Base64;
+use Mail::Sender;
+use Crypt::OpenSSL::Random;
+use Digest::SHA qw(sha256_hex);
 
 hook 'before' => sub {
 	# if we're not a user and we're not a sensor, force the login page
-	if (! session('user') && request->path_info !~ m{^/login}) {
+	if (! session('callsign') && 
+	    request->path_info !~ m{^/login} && 
+	    request->path_info !~ m{^/validate} && 
+	    request->path_info !~ m{^/signup}) {
 		var requested_path => request->path_info;
 		request->path_info('/login');
 	} else {
@@ -31,15 +39,139 @@ post '/login' => sub {
 
 	my $dbh = database();
 	my $loginh = $dbh->prepare_cached("select * from people
-                                        where callsign = ? and password = ?");
+                                        where callsign = ?");
 
-	$loginh->execute(param('name'), param('pass'));
-	if (my $row = $loginh->fetchrow_arrayref()) {
+	$loginh->execute(uc(param('callsign')));
+	if (my $row = $loginh->fetchrow_hashref()) {
+		my $salt = $row->{'salt'};
+		if (get_salted_password($salt, param('password')) ne $row->{'password'}) {
+			debug("here: " . param('password') . " ne $row->{'password'}");
+			return redirect '/login?failed=1';
+		}
+
+		debug($row);
+		# make sure they clicked on the link
+		if (length($row->{'validation'}) > 0) {
+			return template 'login' => {notvalidated => 1};
+		}
 		session user => $row->{'id'};
-		session login => param('name');
+		session callsign => uc(param('callsign'));
 	} else {
+		debug("user not found: " . uc(param('callsign')));
 		return redirect '/login?failed=1';
 	}
+	redirect '/';
 };
+
+get '/signup' => sub {
+	template 'signup';
+};
+
+post '/signup' => sub {
+	
+	my $results = 
+	  dfv({ required => ['firstname', 'lastname', 'callsign', 'email', 'password'],
+	        filters => 'trim',
+	        constraint_methods => 
+	        {
+	         firstname      => qr/^[a-zA-Z]+$/,
+	         lastname       => qr/^[a-zA-Z]+$/,
+	         callsign       => qr/^[a-zA-Z]{1,2}[0-9][a-zA-Z]{1,3}$/,
+	         email  		=> email(),
+	         password       => FV_min_length(6),
+	        }
+	      });
+
+	if ($results->has_invalid || $results->has_missing) {
+		return template 'signup' => { messages => $results->msgs };
+	}
+
+	my $vals = $results->valid;
+	$vals->{'callsign'} = uc($vals->{'callsign'});
+
+	my $dbh = database();
+
+	# see if they already have an account
+	my $sth = $dbh->prepare_cached("select * from people
+                                     where callsign = ?");
+	$sth->execute($vals->{'callsign'});
+	if ($sth->fetchrow_arrayref()) {
+		# they already have an account
+		return template 'signup' => 
+		  { messages => { callsign => 'An account with this callsign already exists' }}
+	}
+
+	# put them into the db
+	my $insh = $dbh->prepare_cached("insert into people (callsign, firstname, lastname, salt, password, email, validation)
+                                                  values(?, ?, ?, ?, ?, ?, ?)");
+	my $salt = get_salt();
+	my $verifyCode = get_salt();
+	$insh->execute($vals->{'callsign'}, $vals->{'firstsname'}, $vals->{'lastname'},
+	               $salt, get_salted_password($salt, $vals->{'password'}),
+	               $vals->{'email'}, $verifyCode);
+
+	# send email
+	my $sender = new Mail::Sender { smtp => 'dawn.hardakers.net',
+	                                  port => 465,
+	                                    auth => 'PLAIN',
+	                                      from => "wes\@ws6z.com"};
+	my $ret = 
+	$sender->MailMsg({ to => $vals->{'email'},
+	                   subject => 'validation link for the simplex mapping site',
+	                   msg =>
+"
+Someone has created an account on the simplex mapping system using this email
+address and a callsign of '$vals->{callsign}'.  Hopefully this was you!
+To validate your address and complete your account, please click on this link:
+
+  http://localhost:4000/validate?code=$verifyCode&callsign=$vals->{callsign}
+
+"
+	                 });	
+
+	if (ref($ret) ne 'Mail::Sender') {
+		warn("mail::sender failed: $ret -- $sender->{error_msg}"); 
+		my $delh = $dbh->prepare_cached("delete from people where callsign = ?");
+		$delh->execute($vals->{'callsign'});
+	}
+
+	template 'waiting_verification';
+};
+
+any '/validate' => sub {
+	
+	my $sth = database()->prepare_cached("select * from people
+                                           where callsign = ? and validation = ?");
+	$sth->execute(uc(param('callsign')), param('validation'));
+
+	debug("validating: " . param('callsign') . " with " . param('validation'));
+
+	if (my $row = $sth->fetchrow_hashref()) {
+		# success
+		my $uph = database()->prepare_cached("update people set validation = ? where callsign = ?");
+		$uph->execute(param('validation'), uc(param('callsign')));
+
+		# set their session
+		session user => $row->{'id'};
+		session callsign => uc(param('callsign'));
+
+		return redirect '/';
+	}
+
+	# failure
+	template 'waiting_verification' => { failed => 1 };
+};
+
+
+sub get_salt {
+	my $salt = encode_base64(Crypt::OpenSSL::Random::random_bytes(10));
+	chomp($salt);
+	return $salt;
+}
+
+sub get_salted_password {
+	my ($salt, $pw) = @_;
+	return sha256_hex($salt . $pw);
+}
 
 1;

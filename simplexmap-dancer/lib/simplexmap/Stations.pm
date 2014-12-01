08d324a8a9920883cc3adfccd52f312174f29ee1
Wes Hardaker <opensource@hardakers.net>
2014-11-30 23:09:37 -0800

    create stations works

--- simplexmap-dancer/lib/simplexmap/Stations.pm
+++ simplexmap-dancer/lib/simplexmap/Stations.pm
@@ -2,17 +2,52 @@ package simplexmap::Stations;
 
 use Dancer ':syntax';
 use Dancer::Plugin::Database;
+use Dancer::Plugin::DataFormValidator;
+use Data::FormValidator::Constraints qw(:closures);
 
 get '/stations' => sub {
 	my $listh = database()->prepare_cached("select * from locations where locationperson = ?");
-	$listh->execute(session('id'));
+	$listh->execute(session('user'));
 	my $list = $listh->fetchall_arrayref({});
+	debug("---------------------");
+	debug($list);
 	
 	template 'stations' => { list => $list }; 
 };
 
 get '/stations/new' => sub {
 	template 'stations/new';
+};
+
+post '/stations' => sub {
+	debug("-------------- here top");
+
+	my $results = 
+	  dfv({ required => ['latitude', 'longitude', 'identifier'],
+	        filters => 'trim',
+	        constraint_methods => 
+	        {
+	         latitude       => qr/^[-+]?[0-9]+\.[0-9]+$/,
+	         longitude      => qr/^[-+]?[0-9]+\.[0-9]+$/,
+	        }
+	      });
+
+	if ($results->has_invalid || $results->has_missing) {
+		debug("fail");
+		debug($results->msgs);
+		return template 'stations/new' => { messages => $results->msgs };
+	}
+
+	my $vals = $results->valid;
+
+	debug("-------------- here");
+
+	my $insh = database()->prepare_cached("
+       insert into locations (locationperson, locationname, locationlat, locationlon)
+                      values (?, ?, ?, ?)");
+	$insh->execute(session('user'), $vals->{'identifier'}, $vals->{'latitude'}, $vals->{'longitude'});
+
+	redirect '/stations';
 };
 
 1;
